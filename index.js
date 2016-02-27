7007e62820758a959d958518fe883c1b854a29c5
Steven Miyakawa SAM <samprofessional@gmail.com>
2016-02-26 22:27:40 -0800

    MVP - Hides new Facebook reactions popups

---
+++ index.js
@@ -0,0 +1,5 @@
+var styleNode = document.createElement('style');
+var styles = document.createTextNode('._iu- { display: none; }');
+
+styleNode.appendChild(styles);
+document.head.appendChild(styleNode);
